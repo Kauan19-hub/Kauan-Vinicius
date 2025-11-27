7354ee4ea44524afcc725f226425e57aa980384d
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2025-11-27 09:05:38 -0300

    IP

--- Front-End/smartCity/src/services/auth.js
+++ Front-End/smartCity/src/services/auth.js
@@ -4,7 +4,7 @@ export async function login(username, password) {
     const response = await axios.post("http://127.0.0.1:8000/api/token/", {
         username: username,
         password: password
+        
     });
-
     return response.data;
 }
